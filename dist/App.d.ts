6adc2336a87a8773ce7f4b93cad7d8ced7a450c8
jinWoo_Lee <dlwlsdn201@naver.com>
2022-03-06 16:39:16 +0900

    [enhancement] 1. bootstrap 설치 + 2. react-bootstrap-icons 설치 + 3. 간단한 예제 구현 및 실습

---
+++ dist/App.d.ts
@@ -0,0 +1,3 @@
+export default App;
+declare function App(): JSX.Element;
+//# sourceMappingURL=App.d.ts.map
---
+++ dist/conponents/Flexbox.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import '../styles/Flexbox/index.css';
+declare const Flexbox: () => JSX.Element;
+export default Flexbox;
+//# sourceMappingURL=Flexbox.d.ts.map
